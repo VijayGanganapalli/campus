1424a5d44afb85ab174408ef8fd23a9ee51b3bca
Vijay <vijayganganapalli@gmail.com>
2019-03-30 21:55:57 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() => runApp(CampusApp());
+
+class CampusApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Center(
+        child: Text('Hey there!'),
+      ),
+    );
+  }
+}
